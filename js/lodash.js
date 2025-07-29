e4869ef6871e0cf786a3718d8e497ce34a79d78d
Rostyslav-afk <110056857+Rostyslav-afk@users.noreply.github.com>
2025-07-29 13:54:00 +0200

    lodashSolo

---
+++ js/lodash.js
@@ -0,0 +1,26 @@
+// const result = _.add(2,3);
+// console.log(result);
+
+
+// window.addEventListener("scroll", _.throttle(() => {
+//     console.log("Виклик обробника подій прокручування кожні 300 мс");
+
+// }, 300), 
+// );
+
+// document.querySelector("input").addEventListener("input", _.debounce(() => {
+//     console.log("Виклик обробника подій введення після 2300 мс бездіяльності після вибуху");
+
+// }, 2300),
+// );
+
+
+// document.addEventListener("scroll", _.debounce(() => {
+//     console.log("Виклик обробника прокручування на кожному запуску потоку подій");
+// }, 300,
+//     {
+//         leading: true,
+//         trailing: false,
+//     }
+// )
+// );
